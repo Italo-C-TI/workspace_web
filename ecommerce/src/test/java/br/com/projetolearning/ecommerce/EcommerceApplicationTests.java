4f1303b7583fea484e85e758652078ec00f523ae
Italo-C-TI <costa.italo.ti@gmail.com>
2022-03-09 13:41:51 -0300

    primeiro commit

---
+++ ecommerce/src/test/java/br/com/projetolearning/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetolearning.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
